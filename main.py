08bd84544ea65acdefc64b43d40c435209bdd508
mizamarzes <mizamarzes@gmail.com>
2024-01-04 15:54:28 -0500

    inicio

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Menu Campuslands: ")
+print("1. Registrar nuevo estudiante")
+print("2. Mostrar lista de estudiantes")
+option=int(input())
+
+if option==1:
+    print() 
+elif option==2:
+    print()
+else: 
+    print("Invalido")
